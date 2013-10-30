d700472a7eb51f221016a4105cc454dd09c30468
hellbinder <miguel_1216@yahoo.com>
2013-10-30 16:20:14 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="node-vsdoc.js" />
+/* Add Module Dependencies */
+
+var express = require("express")
+, stylus = require("stylus")
+, nib = require("nib");
+
+
+var app = express();
+function compile(str, path) {
+    return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(stylus.middleware(
+  {
+      src: __dirname + '/public'
+  , compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function (req, res) {
+    res.end("This should be working!");
+});
+app.listen(3000);
