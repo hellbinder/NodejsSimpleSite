31b0adad54eaab81df0f153093ca81eac5773584
name <email>
2013-12-05 02:08:46 -0500

    Improved css. Chat js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,21 +1,14 @@
-var username;
+var username, currentUserName, socket;
 function ConnectToChat() {
     var messages = [];
-    var socket = io.connect();
+    socket = io.connect();
     var field = document.getElementById("messageInput");
     var sendButton = document.getElementById("send");
-    var content = document.getElementById("messages");
+    var content = document.getElementById("chatEntries");
 
     socket.on('message', function (data) {
         console.log(data);
-        if(data.message) {
-            // messages.push([data.username, data.message]);
-            // var html = '';
-            // for(var i=0; i<messages.length; i++) {
-            //     html += messages[i][0] +": " + messages[i][1] + '<br />';
-            // }
-            // content.innerHTML = html;
-            // console.log(messages);
+        if(data.message) {;
             CreateMessage(data.username, data.message);
             $(content).animate({
                 scrollTop: content.scrollHeight
@@ -41,26 +34,60 @@ function ConnectToChat() {
 
 }
 
-jQuery(document).ready(function($) {
-    $('#modalUsername').modal('show');
+$(document).ready(function() {
+  //Event triggered when modal is shown
+  $('#usernameModal').on('shown.bs.modal', function(){
     $("#username").focus();
-    $("#usernameForm").submit(function(e){
-        e.preventDefault(); //Prevent default of form (which is to postback)
-        var currentUserName = $("#username").val();
-        if(currentUserName != "")
-        {
-            // debugger;
-            username = $("#username").val();
-            $('#modalUsername').modal('hide');
-            ConnectToChat();
-            $("#messageInput").focus();
-        }
-    });
+  });
+  $('#usernameModal').modal('show');
+  $("#usernameForm").submit(function(e){
+    e.preventDefault(); //Prevent default of form (which is to postback)
+    currentUserName = $("#username").val();
+    if(currentUserName != "")
+      {
+        // debugger;
+        username = $("#username").val();
+        $('#usernameModal').modal('hide');
+        ConnectToChat();
+        $("#messageInput").focus();
+      }
+  });
+
+  //Do for testing
+  // SetUpInitalLogin();
+
+  //Put custom scrollbar
+  // NOT WORKING
+   //$("#chatEntries").mCustomScrollbar();
+
 });
 
 function CreateMessage(username, message)
 {
-    var userMessage = $('<li class="message" style="display:none"><strong>' + username + '</strong>: ' + message + '</li>')
+    var userMessage = $('<li class="message" style="display:none"><strong>' + username + '</strong>: ' + message + '</li>');
+    var messageClass;
+    switch(username)
+    {
+      case "Server":
+        messageClass = "messageServer"
+        break;
+      case currentUserName:
+        messageClass = "messageSelf"
+        break;
+      default:
+        messageClass = "messageOther"
+    }
+    userMessage.addClass(messageClass);
     $("#messages ul").append(userMessage);
     userMessage.fadeIn();
+}
+
+function SetUpInitalLogin () {
+  $("#username").val("Mickey");
+  $("#btnUsername").trigger("click");
+  socket.emit('send', { username: "Mickey", message: "WAZAAAAAAAAA" });
+  socket.emit('send', { username: "Hilox", message: "Well this is working. For now." });
+  socket.emit('send', { username: "Berthlax", message: 'Well as it should. DUH' });
+  socket.emit('send', { username: "Hilox", message: "No it shouldnt!. THIS IS A TEST!" });
+  socket.emit('send', { username: "Mickey", message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fama timiditatem habeatur epicuri percipitur nemore attingere sequitur quidem fecisse "});
 }
